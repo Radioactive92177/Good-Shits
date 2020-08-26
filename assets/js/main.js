a001ed78d2c3e40e92889297655f194ad38d3d1c
Radioactive92177 <Raju.11811924@lpu.in>
2020-08-26 23:58:06 +0530

    Good shits working

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+  const number = document.querySelector("#numberOfJokes").value;
+  const firstName = document.querySelector("#firstName").value;
+  const lastName = document.querySelector("#lastName").value;
+
+  const xmr = new XMLHttpRequest();
+
+  xmr.open(
+    "GET",
+    `http://api.icndb.com/jokes/random/${number}?firstName=${firstName}&lastName=${lastName}`,
+    true
+  );
+
+  xmr.onload = function () {
+    let output = "";
+    let data = JSON.parse(this.responseText);
+    data.value.forEach(function (joke) {
+      output += `
+        <ul>
+            <li><h6 style="font-weight:bolder">${joke.joke}<h6></li>
+        </ul>
+        `;
+    });
+    document.querySelector("#space").innerHTML = output;
+  };
+
+  xmr.send();
+
+  e.preventDefault();
+});
