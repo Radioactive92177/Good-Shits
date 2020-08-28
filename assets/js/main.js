f2868f2dec545b98c77e676065d206aadcaac481
Radioactive92177 <Raju.11811924@lpu.in>
2020-08-28 14:29:37 +0530

    Changed to Fetch API Managament standard

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,28 +3,27 @@ document.querySelector("form").addEventListener("submit", function (e) {
   const firstName = document.querySelector("#firstName").value;
   const lastName = document.querySelector("#lastName").value;
 
-  const xmr = new XMLHttpRequest();
-
-  xmr.open(
-    "GET",
-    `https://api.icndb.com/jokes/random/${number}/limitTo=explicit?firstName=${firstName}&lastName=${lastName}`,
-    true
-  );
-
-  xmr.onload = function () {
-    let output = "";
-    let data = JSON.parse(this.responseText);
-    data.value.forEach(function (joke) {
-      output += `
-        <ul>
-            <li><h6 style="font-weight:bolder">${joke.joke}<h6></li>
-        </ul>
-        `;
+  fetch(
+    `https://api.icndb.com/jokes/random/${number}/limitTo=explicit?firstName=${firstName}&lastName=${lastName}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let output = "";
+      data.value.forEach(function (joke) {
+        output += `
+            <ul>
+                <li><h6 style="font-weight:bolder">${joke.joke}<h6></li>
+            </ul>
+            `;
+      });
+      document.querySelector("#space").innerHTML = output;
+    })
+    .catch(function (err) {
+      let errHTML = `<h5 style="color:red">${err}<h5>`;
+      document.querySelector("#space").innerHTML = errHTML;
     });
-    document.querySelector("#space").innerHTML = output;
-  };
-
-  xmr.send();
 
   e.preventDefault();
 });
