424ac95cbd62eaab06fa1c7e5187dbb7af0bdf05
Radioactive92177 <Raju.11811924@lpu.in>
2020-08-29 01:03:25 +0530

    Async and Await Applied

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,39 @@
+// Calling API with Async and await
+async function getShits(number, firstName, lastName) {
+  const response = await fetch(
+    `https://api.icndb.com/jokes/random/${number}/limitTo=explicit?firstName=${firstName}&lastName=${lastName}`
+  );
+  const resposeData = await response.json();
+  return resposeData;
+}
+
 document.querySelector("form").addEventListener("submit", function (e) {
   const number = document.querySelector("#numberOfJokes").value;
   const firstName = document.querySelector("#firstName").value;
   const lastName = document.querySelector("#lastName").value;
 
-  fetch(
-    `https://api.icndb.com/jokes/random/${number}/limitTo=explicit?firstName=${firstName}&lastName=${lastName}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      let output = "";
-      data.value.forEach((joke) => {
-        output += `
-            <ul>
-                <li><h6 style="font-weight:bolder">${joke.joke}<h6></li>
-            </ul>
-            `;
-      });
-      document.querySelector("#space").innerHTML = output;
-    })
-    .catch((err) => {
-      let errHTML = `<h5 style="color:red">${err}<h5>`;
-      document.querySelector("#space").innerHTML = errHTML;
-    });
+  // API call
+  const shits = getShits(number, firstName, lastName);
 
+  // Showing the data
+  shits.then((data) => {
+    let output = "";
+    data.value.forEach((shit) => {
+      output += `
+        <ul>
+          <li><h6 style="font-weight:bolder">${shit.joke}</h6></li>
+        </ul>
+      `;
+    });
+    document.querySelector("#space").innerHTML = output;
+  });
+
+  // Catching the error
+  shits.catch((err) => {
+    let errHTML = `<h5 style="color:red">${err}<h5>`;
+    document.querySelector("#space").innerHTML = errHTML;
+  });
+
+  // Stopping from flashing the data
   e.preventDefault();
 });
