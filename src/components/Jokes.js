9cc99d372460724e8295a010a768c7eb237b7281
MG Raju <raju.11811924@lpu.in>
2020-10-22 23:50:49 +0530

    John Doe defaults done

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import axios from "axios";
+import PropTypes from "prop-types";
+
 import Joke from "./Joke";
-const Jokes = () => {
+
+const Jokes = ({ numberOfJokes, firstName, lastName }) => {
   const [jokes, setjokes] = useState([]);
 
   useEffect(() => {
     const fetchJokes = async () => {
       const { data } = await axios.get(
-        `https://api.icndb.com/jokes/random/3/limitTo=explicit?firstName=Chuck&lastName=Norris`
+        `https://api.icndb.com/jokes/random/${numberOfJokes}/limitTo=explicit?firstName=${firstName}&lastName=${lastName}`
       );
       setjokes(data.value);
     };
@@ -22,6 +25,17 @@ const Jokes = () => {
       ))}
     </Container>
   );
+};
+
+Jokes.defaultProps = {
+  numberOfJokes: 3,
+  firstName: "John",
+  lastName: "Doe",
+};
+Jokes.propTypes = {
+  numberOfJokes: PropTypes.number.isRequired,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
 };
 
 export default Jokes;
