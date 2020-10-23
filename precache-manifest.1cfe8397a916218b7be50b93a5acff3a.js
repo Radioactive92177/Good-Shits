5fb54aafd6cb51f3b1dccc027a3060fc742dc875
MG Raju <raju.11811924@lpu.in>
2020-10-23 13:37:56 +0530

    Updates

---
+++ precache-manifest.1cfe8397a916218b7be50b93a5acff3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e16cbf0f2e9bc3034e7a0a4f87eed67",
+    "url": "/Good-Shits/index.html"
+  },
+  {
+    "revision": "d997b158f0c024e16e45",
+    "url": "/Good-Shits/static/css/main.806e3354.chunk.css"
+  },
+  {
+    "revision": "52857429c6dede853a19",
+    "url": "/Good-Shits/static/js/2.f5330e80.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/Good-Shits/static/js/2.f5330e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d997b158f0c024e16e45",
+    "url": "/Good-Shits/static/js/main.bf17698c.chunk.js"
+  },
+  {
+    "revision": "2816b9fe2ba6e1968cc9",
+    "url": "/Good-Shits/static/js/runtime-main.6601013c.js"
+  }
+]);
